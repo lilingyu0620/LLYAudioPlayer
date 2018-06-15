51fc54757e5231020c6377b4153b8e7682240eef
lly <bjlilingyu@corp.netease.com>
2018-06-15 10:30:48 +0800

    添加注释

--- LLYAudioPlayer/LLYAudioPlayer/FileManager/LLYFileManager.h
+++ LLYAudioPlayer/LLYAudioPlayer/FileManager/LLYFileManager.h
@@ -14,9 +14,9 @@
 + (NSString *)pathWithUrl:(NSString *)fileUrl;
 //将数据保存到本地目录下
 + (BOOL)saveFileWithPath:(NSString *)path fileObject:(id)fileObject;
-
+//判断当前url对应的文件是否已经缓存
 + (BOOL)isFileExit:(NSString *)url;
-
+//获取文件大小
 + (unsigned long long)fileSizeWithFilePath:(NSString *)filePath;
 
 @end
